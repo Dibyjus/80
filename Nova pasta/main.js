2154e10b7d70641d2546ea50a88acae693784d64
Dibyjus <113060789+Dibyjus@users.noreply.github.com>
2023-03-01 18:07:25 -0300

    Add files via upload

---
+++ Nova pasta/main.js
@@ -0,0 +1,45 @@
+var nameOfPeople = []
+
+function submit()
+{
+var GuestName = document.getElementById("name1").value;
+namesOfPeople.push(GuestName);
+
+console.log(GuestName);
+
+console.log(namesOfPeople);
+
+var lenghtOfName = namesOfPeople.length;
+ console.log(lenghtOfName);
+  document.getElementById("displayName").innerHTML=namesOfPeople.toString(); 
+
+}
+
+function sorting() {
+     namesOfPeople.sort();
+      var i= namesOfPeople.join("<br>");
+ console.log(namesOfPeople);
+  document.getElementById("sorted").innerHTML=i.toString();
+ }
+
+function show()
+{
+var i=namesOfPeoples.join("<br>");
+console.log(namesOfPeoples);
+document.getElementById("p1").innerHTML=i.toString();
+document.getElementById("sortButton").style.display="block"
+
+}
+function searching() { 
+    var s= document.getElementById("s1").value;
+     var found=0;
+      var j;
+       for(j=0; j<namesOfPeople.length; j++) 
+       {
+         if(s==namesOfPeople[j]){
+             found=found+1; 
+            } 
+        }
+ document.getElementById("p2").innerHTML="Nome encontrado "+found+" vez(es)";
+  console.log("Nome encontrado "+found+" vez(es)");
+ }
